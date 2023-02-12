3f60dcd18425d72e0d4c1d612d01bfd768f1d14b
KANNAPPANS <Kannappan.Somasundaram@thetrainline.com>
2023-02-12 22:48:54 +0000

    Phase 1 refactor

--- Chapter1/PlaysChargeCalculator.cs
+++ Chapter1/PlaysChargeCalculator.cs
@@ -4,49 +4,87 @@ namespace RefactoringBookExamples;
 public class PlaysChargeCalculator
 {
     public string statement (Invoice invoice, Dictionary<string, Play> plays) {
-        var totalAmount = 0;
-        var volumeCredits = 0;
         var result = $"Statement for {invoice.Customer}\n";
+        foreach (var perf in invoice.Performances)
+        {
+            // print line for this order
+            result += $"  {PlayFor(perf).Name}: ${ToUSD(amountFor(perf))} ({perf.Audience} seats)\n";
+        }
+        result += $"Amount owed is {ToUSD(GetTotalAmount())}\n";
+        result += $"You earned {TotalVolumeCredits()} credits\n";
+        return result;
         
-        foreach (var perf in invoice.Performances) {
-            var play = plays[perf.PlayID];
-            var thisAmount = 0;
-
-            switch (play.Type) {
+        Play PlayFor(Performance performance)
+        {
+            return plays[performance.PlayID];
+        }
+        string ToUSD(int thisAmount)
+        {
+            return (thisAmount/100).ToString("C");
+        }
+        
+        int amountFor(Performance aPerformance)
+        {
+            int result;
+            switch (PlayFor(aPerformance).Type)
+            {
                 case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.Audience > 30) {
-                        thisAmount += 1000 * (perf.Audience - 30);
-                    }
-                    break;
+                result = 40000;
+                if (aPerformance.Audience > 30)
+                {
+                    result += 1000 * (aPerformance.Audience - 30);
+                }
+
+                break;
                 case "comedy":
-                    thisAmount = 30000;
-                    if (perf.Audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.Audience - 20);
-                    }
-                    thisAmount += 300 * perf.Audience;
-                    break;
+                result = 30000;
+                if (aPerformance.Audience > 20)
+                {
+                    result += 10000 + 500 * (aPerformance.Audience - 20);
+                }
+
+                result += 300 * aPerformance.Audience;
+                break;
                 default:
-                    throw new Exception($"unknown Type: {play.Type}");
+                throw new Exception($"unknown Type: {PlayFor(aPerformance).Type}");
             }
 
-            // add volume credits
-            volumeCredits += Math.Max(perf.Audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy" == play.Type) 
-                volumeCredits += (int)Math.Floor((decimal)(perf.Audience / 5));
-
-            // print line for this order
-            result += $"  {play.Name}: ${format(thisAmount/100)} ({perf.Audience} seats)\n";
-            totalAmount += thisAmount;
+            return result;
         }
-        result += $"Amount owed is {format(totalAmount/100)}\n";
-        result += $"You earned {volumeCredits} credits\n";
-        return result;
+        
+        int VolumeCreditsFor(Performance aPerformance)
+        {
+            int result;
+            result = Math.Max(aPerformance.Audience - 30, 0);
+            // add extra credit for every ten comedy attendees
+            if ("comedy" == PlayFor(aPerformance).Type)
+                result += (int)Math.Floor((decimal)(aPerformance.Audience / 5));
+            return result;
+        }
+
+        int TotalVolumeCredits()
+        {
+            var volumeCredits = 0;
+            foreach (var perf in invoice.Performances)
+            {
+                // add volume credits
+                volumeCredits += VolumeCreditsFor(perf);
+            }
+
+            return volumeCredits;
+        }
+
+        int GetTotalAmount()
+        {
+            var totalAmount = 0;
+            foreach (var perf in invoice.Performances)
+            {
+                totalAmount += amountFor(perf);
+            }
+
+            return totalAmount;
+        }
     }
 
-    private string format(int thisAmount)
-    {
-        return thisAmount.ToString("C");
-    }
+
 }
